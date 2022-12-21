3dddbf622fc412025cae17c28b5c32c98b6a881c
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-12-21 16:34:34 +0530

    create login screen file

---
+++ PrivateFileSharingApp/LoginScreen.swift
@@ -0,0 +1,20 @@
+//
+//  LoginScreen.swift
+//  PrivateFileSharingApp
+//
+//  Created by Bhumika Patel on 21/12/22.
+//
+
+import SwiftUI
+
+struct LoginScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginScreen()
+    }
+}
