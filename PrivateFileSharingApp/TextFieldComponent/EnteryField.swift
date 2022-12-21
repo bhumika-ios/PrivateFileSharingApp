2e7e14e7637249cacd87e667d5402ab0a56cb619
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-12-21 16:36:30 +0530

    create group textfieldComponent- add eneryfield file

---
+++ PrivateFileSharingApp/TextFieldComponent/EnteryField.swift
@@ -0,0 +1,20 @@
+//
+//  EnteryField.swift
+//  PrivateFileSharingApp
+//
+//  Created by Bhumika Patel on 21/12/22.
+//
+
+import SwiftUI
+
+struct EnteryField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EnteryField_Previews: PreviewProvider {
+    static var previews: some View {
+        EnteryField()
+    }
+}
