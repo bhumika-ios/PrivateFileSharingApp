44dcba9033817c959689765120e1421bb7d472c7
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-12-21 16:55:59 +0530

    add text - welcomnback - set size

--- PrivateFileSharingApp/LoginScreen.swift
+++ PrivateFileSharingApp/LoginScreen.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct LoginScreen: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("Welcome Back")
+                .font(.system(size: 32))
+        }
     }
 }
 
