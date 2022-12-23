f244d374949b1fa5ff368a24fa896aff28a09cf7
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-12-23 16:10:48 +0530

    add button with sf image

--- PrivateFileSharingApp/LoginScreen.swift
+++ PrivateFileSharingApp/LoginScreen.swift
@@ -11,15 +11,48 @@ struct LoginScreen: View {
     @ObservedObject var fieldVM = FieldViewModel()
     var body: some View {
         VStack{
+            
+//MARK:- Text add and set font size
             Text("Welcome Back!")
                 .font(.system(size: 32).bold())
             Text("Please type your email to log in")
                 .font(.system(size: 22))
-                
             VStack{
-                TextFieldComponent(placeHolder: "Email Address", field: $fieldVM.email)
+                VStack{
+                    TextFieldComponent(placeHolder: "Email Address", field: $fieldVM.email)
+                }
+                .padding()
+                Button(action: {
+                    
+                    
+                }) {
+                    HStack{
+                        Text("Next Step ")
+                            .foregroundColor(.black)
+                            .font(.system(size: 28))
+                        Image(systemName: "chevron.forward")
+                            .resizable()
+                            .frame(width: 15, height: 20)
+                            .foregroundColor(.green)
+                            .padding(.vertical, 5)
+                            .padding(.horizontal)
+                        //  .background(Capsule().fill(Color.blue))
+                    }
+                    .frame(width:360 ,height:70)
+                    .backgroundStyle(Color(UIColor.secondarySystemBackground))
+                    .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.black.opacity(1), lineWidth: 1))
+                    .fixedSize(horizontal: false, vertical: true)
+                }
+                .padding()
+                Button(action: {
+                    
+                })
+                {
+                    HStack{
+                        Text("")
+                    }
+                }
             }
-            .padding()
         }
     }
 }
