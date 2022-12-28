0925d1d7a02b8d0f12b1fa1624f2f79c599e2103
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-12-28 17:07:44 +0530

    add image and text for list view display of properly

--- PrivateFileSharingApp/View/FileListView.swift
+++ PrivateFileSharingApp/View/FileListView.swift
@@ -9,12 +9,32 @@ import SwiftUI
 
 struct FileListView: View {
     var body: some View {
-        HStack{
-            Image(systemName: "camera")
-                .resizable()
-                .scaledToFit()
-                .frame()
+        NavigationView{
+            List(0 ..< 20){ item in
+                HStack{
+                    Image(systemName: "camera")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 70)
+                        .cornerRadius(4)
+                    VStack(alignment: .leading, spacing: 2){
+                        Text("fgbfgbdfgdfgb")
+                            .font(.custom("Genos-Regular", size: 24))
+                            .fontWeight(.semibold)
+                            .lineLimit(2)
+                            .minimumScaleFactor(0.5)
+                        Text("january 1, 2022")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                    .padding()
+                }
+                .padding()
+            }
+           
+            .navigationTitle("Files")
         }
+       
     }
 }
 
