306a7f11e40e13caf383fe7aa5d0d0ef62520b00
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-12-28 13:02:03 +0530

    change validation code in register screen - used view model class with validation function

--- PrivateFileSharingApp/View/RegistrationScreen.swift
+++ PrivateFileSharingApp/View/RegistrationScreen.swift
@@ -9,82 +9,80 @@ import SwiftUI
 
 struct RegistrationScreen: View {
     @ObservedObject var fieldVM = FieldViewModel()
-    @ObservedObject var emailObj = EmailValidationobj()
-    @ObservedObject var passObj = PasswordValidationobj()
-    @ObservedObject var conPassObj = ConPasswordValidationobj()
-    @ObservedObject var nameObj = NameValidationobj()
     var body: some View {
-        VStack{
+        ScrollView{
             VStack{
-                
-               TxtHeadingComponent(title: "Create New Account")
-               
-               TxtTagComponent(subTitle: "Please fill registration to create account")
-                   
-            }
-            .padding(.vertical,60)
-            Group{
-                VStack(alignment:.leading){
-                    TxtTagComponent(subTitle: "Email")
-                    TextFieldComponent(
-                        placeHolder: "Email Address",
-                        field: $emailObj.email
-                    )
-                    TxtErrorComponent(error: emailObj.error)
+                VStack{
                     
-                    TxtTagComponent(subTitle: "Full Name")
-                    TextFieldComponent(placeHolder: "Full Name", field: $nameObj.fullName)
-                    TxtErrorComponent(error: nameObj.error)
+                    TxtHeadingComponent(title: "Create New Account")
                     
-                    TxtTagComponent(subTitle: "Create Password")
-                    TextFieldComponent(placeHolder: "Password", field:$passObj.password, isSecure: true)
-                    TxtErrorComponent(error: passObj.error)
+                    TxtTagComponent(subTitle: "Please fill registration to create account")
                     
-                    //TxtTagComponent(subTitle: "Confirm Password")
-                    //TextFieldComponent(placeHolder: "Confirm Password", field: $conPassObj.conPassword, isSecure: true)
-                    //TxtErrorComponent(error: conPassObj.error)
-                    
-                    }
-                .padding()
                 }
-            VStack(alignment:.leading){
-                TxtTagComponent(subTitle: "Confirm Password")
-                TextFieldComponent(
-                    placeHolder: "Confirm Password",
-                    field: $fieldVM.conPassword,
-                    isSecure: true)
-                TxtErrorComponent(error: conPassObj.error)
-            }
-            
-            .padding()
-            .padding(.vertical,-30)
-            //.padding()
-            VStack{
-                Text("By clicking on 'Create Account' button I aggree ")
-                HStack{
-                    Text("to the")
-                    BtnTextComponent(action: {}){
+                .padding(.vertical,60)
+                Group{
+                    VStack(alignment:.leading){
+                        TxtTagComponent(subTitle: "Email")
+                        TextFieldComponent(
+                            placeHolder: "Email Address",
+                            field: $fieldVM.email
+                        )
+                        TxtErrorComponent(error: fieldVM.emailPrompt)
+                        
+                        TxtTagComponent(subTitle: "Full Name")
+                        TextFieldComponent(placeHolder: "Full Name", field: $fieldVM.fullName)
+                        TxtErrorComponent(error: fieldVM.namePrompt)
+                        
+                        TxtTagComponent(subTitle: "Create Password")
+                        TextFieldComponent(placeHolder: "Password", field:$fieldVM.password, isSecure: true)
+                        TxtErrorComponent(error:fieldVM.passwordPrompt)
+                        
+                        //TxtTagComponent(subTitle: "Confirm Password")
+                        //TextFieldComponent(placeHolder: "Confirm Password", field: $conPassObj.conPassword, isSecure: true)
+                        //TxtErrorComponent(error: conPassObj.error)
+                        
+                    }
+                    .padding()
+                }
+                VStack(alignment:.leading){
+                    TxtTagComponent(subTitle: "Confirm Password")
+                    TextFieldComponent(
+                        placeHolder: "Confirm Password",
+                        field: $fieldVM.conPassword,
+                        isSecure: true)
+                    TxtErrorComponent(error: fieldVM.confirmPwPrompt)
+                }
+                
+                .padding()
+                .padding(.vertical,-30)
+                //.padding()
+                VStack{
+                    Text("By clicking on 'Create Account' button I aggree ")
+                    HStack{
+                        Text("to the")
+                        BtnTextComponent(action: {}){
                             Text("Terms of Service")
                                 .foregroundColor(.green)
                         }
-                    Text("and")
-                    BtnTextComponent(action: {}){
-                        Text("Privacy Policy")
-                            .foregroundColor(.green)
-                    
+                        Text("and")
+                        BtnTextComponent(action: {}){
+                            Text("Privacy Policy")
+                                .foregroundColor(.green)
+                            
                         }
                     }
                 }
-            .padding(.vertical,10)
-            VStack{
-//                BtnBorderComponent(action: {}) {
-//                    Text("Next Step ")
-//                }
-//                .padding(.vertical,20)
+                .padding(.vertical,10)
+                VStack{
+                                    BtnBorderComponent(action: {}) {
+                                        Text("Next Step ")
+                                    }
+                                    .padding(.vertical,20)
+                }
+                
+                
+                
             }
-           
-           
-               
         }
     }
 }
