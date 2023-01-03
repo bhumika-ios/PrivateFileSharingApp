eebc5394079bfcae703b07ad0eff8f0a6e3210b1
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2023-01-03 14:12:17 +0530

    create administrationbottomsheet for administration option

--- PrivateFileSharingApp/View/MainView/ProfileSheet.swift
+++ PrivateFileSharingApp/View/MainView/ProfileSheet.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ProfileSheet: View {
-   
+    @State var showingBottomSheet = false
     var body: some View {
         VStack(alignment: .leading){
             Spacer()
@@ -73,11 +73,18 @@ struct ProfileSheet: View {
                             TxtTagComponent(subTitle: "Administration")
                             Spacer()
                             
-                            Button(action: {}){
+                            Button(action: {
+                                showingBottomSheet.toggle()
+                            }){
                                 Image(systemName: "chevron.forward")
                                     .resizable()
                                     .frame(width: 8, height: 18)
                                     .foregroundColor(.gray)
+                            }
+                           
+                            .sheet(isPresented: $showingBottomSheet){
+                                AdministrationSheet()
+                                    .presentationDetents([.height(260),.height(260)])
                             }
                             
                         }
@@ -116,5 +123,114 @@ struct CustomCorner: Shape{
         let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners,cornerRadii: CGSize(width: 35, height: 35))
         
         return Path(path.cgPath)
+    }
+}
+struct AdministrationSheet: View {
+    
+    var body: some View {
+        NavigationStack{
+            VStack(alignment: .leading){
+                Spacer()
+                VStack(spacing:12){
+                    //                Capsule()
+                    //                    .fill(Color.gray)
+                    //                    .frame(width: 60, height: 4)
+//                    HStack{
+//                        Image("profilePic")
+//                            .resizable()
+//                            .frame(width: 60, height: 60)
+//                            .cornerRadius(10)
+//                            .padding()
+//                        VStack(alignment: .leading){
+//                            TxtHeadingComponent(title: "XXX")
+//                            Text("Email Address")
+//                                .font(.custom("Genos-Medium", size: 25))
+//                                .foregroundColor(.green)
+//                            //                        TxtTagComponent(subTitle: "EmailAddress")
+//                            //
+//                        }
+//                        Spacer()
+//                        Image(systemName: "bell")
+//                            .resizable()
+//                            .frame(width: 25, height: 25)
+//                    }
+                   // .padding()
+                  //  ScrollView(.vertical, showsIndicators: false,content: {
+                        VStack(alignment: .leading, spacing: 0){
+                            HStack{
+                                Image(systemName: "cube")
+                                
+                                    .resizable()
+                                    .frame(width: 20, height: 20)
+                                    .padding()
+                                TxtTagComponent(subTitle: "Dashboard")
+                                
+                            }
+                            HStack{
+                                Image(systemName: "person.and.person")
+                               
+                                    .resizable()
+                                    .frame(width: 20, height: 20)
+                                    .padding()
+                                TxtTagComponent(subTitle:  "Users")
+                                Spacer()
+                                
+//                                Button(action: {}){
+//                                    Image(systemName: "chevron.forward")
+//                                        .resizable()
+//                                        .frame(width: 8, height: 18)
+//                                        .foregroundColor(.gray)
+//                                }
+//
+                            }
+                            HStack{
+                                
+                              
+                                
+                                Image(systemName: "gearshape.fill")
+                                    .resizable()
+                                    .frame(width: 20, height: 20)
+                                    .padding()
+                                
+                                TxtTagComponent(subTitle: "Settings")
+                                Spacer()
+                                
+//                                Button(action: {}){
+//                                    Image(systemName: "chevron.forward")
+//                                        .resizable()
+//                                        .frame(width: 8, height: 18)
+//                                        .foregroundColor(.gray)
+//                                }
+                                
+                            }
+                            HStack{
+                                Image(systemName: "display")
+                                
+                                    .resizable()
+                                    .frame(width: 20, height: 20)
+                                    .padding()
+                                TxtTagComponent(subTitle: "Pages")
+                            }
+                            HStack{
+                                Image(systemName: "globe")
+                               
+                                    .resizable()
+                                    .frame(width: 20, height: 20)
+                                    .padding()
+                                TxtTagComponent(subTitle: "Languages")
+                            }
+                        }
+                        .padding(.horizontal)
+                        .padding(.top,10)
+                        .padding(.bottom)
+                        
+                  //  })
+                }
+                .padding(.top)
+                .clipShape(CustomCorner(corners: [.topLeft,.topRight]))
+            }
+            .ignoresSafeArea()
+            //.background(Color.black.opacity(0.3).ignoresSafeArea())
+        }
     }
 }
